e1f24ce66a72c1dc53d04cdb343eb3491ce4be25
Vasundhara Mane <vasundhara.mane@thoughtworks.com>
2022-10-04 15:46:07 +0530

    updated file

--- src/test/java/firstAppiumTest_ApiDemosDebug.java
+++ src/test/java/firstAppiumTest_ApiDemosDebug.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 
 public class firstAppiumTest_ApiDemosDebug {
 
-    //Execute Mobile Command (Executes a native mobile command)
+    //Execute Mobile Command (Executes a native mobile command) -  Used ApiDemosDebug app
     @Test
     public void scrollDown(){
         try {
